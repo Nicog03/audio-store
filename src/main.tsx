8fff03175ad9be8a42bb99964719221fd143e2e2
nicolas <nicolas.lima03@hotmail.com>
2023-07-10 22:49:07 -0300

    define page initial routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./main.css";
+import SignInPage from "./pages/SignInPage.tsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/signin",
+    element: <SignInPage mode={"signin"} />,
+  },
+  {
+    path: "/signup",
+    element: <SignInPage mode={"signup"} />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
