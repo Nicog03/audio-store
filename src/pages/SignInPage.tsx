b88d51f86cd2cf49ed3bb5bc2d37fc191bc610e3
nicolas <nicolas.lima03@hotmail.com>
2023-07-10 21:36:57 -0300

    improve structure & add style classes

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,20 +1,37 @@
+import classes from "./SignInPage.module.css";
+import { Mail, Lock } from "react-feather";
+
 const SignIn = () => {
   return (
-    <>
-      <h1>Sign In</h1>
-      <p>It's modular and designed to last</p>
-      <form action="">
-        <label htmlFor="email">Email</label>
-        <input id="email" type="email" />
-        <label htmlFor="password">Password</label>
-        <input id="password" type="password" />
-        <a href="#">Forgot Password</a>
-        <button>Sign In</button>
-      </form>
-      <p>
-        Don't have an account? <a href="">Sign Up here</a>
-      </p>
-    </>
+    <div className={classes.container}>
+      <hgroup className={classes.header}>
+        <h1 className={classes.h1}>Audio</h1>
+        <p className={classes.h1}>It's modular and designed to last</p>
+      </hgroup>
+      <div className={classes.bottomSection}>
+        <form action="" className={classes.form}>
+          <div className={classes.inputContainer}>
+            <input id="email" type="email" placeholder=" " />
+            <div className={classes.label}>
+              <Mail />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+          <div className={classes.inputContainer}>
+            <input id="password" type="password" placeholder=" " />
+            <div className={classes.label}>
+              <Lock />
+              <label htmlFor="password">Password</label>
+            </div>
+          </div>
+          <a href="#">Forgot Password</a>
+          <button>Sign In</button>
+        </form>
+        <p className={classes.SignUpParagraph}>
+          Don't have an account? <a href="">Sign Up here</a>
+        </p>
+      </div>
+    </div>
   );
 };
 
