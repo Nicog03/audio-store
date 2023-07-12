cd1fc5ae7271267edbcbd1de3d75b14a66ff4936
nicolas <nicolas.lima03@hotmail.com>
2023-07-12 13:03:23 -0300

    move route setup from main to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import HomePage from "./pages/HomePage.tsx";
+import SignInPage from "./pages/SignInPage.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    children: [
+      {
+        path: ":category",
+        element: <HomePage />,
+      },
+    ],
+  },
+  {
+    path: "/signin",
+    element: <SignInPage mode={"signin"} />,
+  },
+  {
+    path: "/signup",
+    element: <SignInPage mode={"signup"} />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <p>Audio Store</p>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.tsx";
-import HomePage from "./pages/HomePage.tsx";
+import App from "./App.tsx";
 import "./main.css";
-import SignInPage from "./pages/SignInPage.tsx";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-    children: [
-      {
-        path: ":category",
-        element: <HomePage />,
-      },
-    ],
-  },
-  {
-    path: "/signin",
-    element: <SignInPage mode={"signin"} />,
-  },
-  {
-    path: "/signup",
-    element: <SignInPage mode={"signup"} />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
