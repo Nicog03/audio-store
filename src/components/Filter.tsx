a5efc0ef576ee462a3bb6b8238ea48161a6266ca
nicolas <nicolas.lima03@hotmail.com>
2023-07-13 23:50:59 -0300

    get data from each of the inputs within & lift it up as well as some extra info

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -4,37 +4,101 @@ import TextInput from "./TextInput";
 import { X } from "react-feather";
 import Button from "./Button";
 import SortOptions from "./SortOptions";
+import { useState } from "react";
+
+interface FilterType {
+  category: string;
+  sortBy: string;
+  minPrice: string;
+  maxPrice: string;
+}
 
 interface PropType {
   XClickAction: React.MouseEventHandler<HTMLButtonElement>;
+  changeData: React.Dispatch<React.SetStateAction<FilterType | null>>;
+  setFilterQnt: React.Dispatch<React.SetStateAction<number>>;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Filter: React.FC<PropType> = ({ XClickAction }) => {
+const Filter: React.FC<PropType> = ({
+  XClickAction,
+  changeData,
+  setFilterQnt,
+  setOpen,
+}) => {
+  const [category, setCategory] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+
+  const changeCategoryHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCategory(e.target.value);
+  };
+
+  const changeSortByHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSortBy(e.target.value);
+  };
+
+  const changeMinPriceHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMinPrice(e.target.value);
+  };
+
+  const changeMaxPriceHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMaxPrice(e.target.value);
+  };
+
+  const changeFilterOptions = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const filter = [category, sortBy, minPrice, maxPrice];
+    let numOfFilter = 0;
+
+    filter.forEach((item) => (item != "" ? numOfFilter++ : null));
+
+    setFilterQnt(numOfFilter);
+    changeData({ category, sortBy, minPrice, maxPrice });
+    setOpen(false);
+  };
+
   return (
-    <div className={classes.container}>
+    <form onSubmit={changeFilterOptions} className={classes.container}>
       <div className={classes.mainHeadingContainer}>
         <h2>Filter</h2>
-        <button onClick={XClickAction} className={classes.closeButton}>
+        <button
+          type="button"
+          onClick={XClickAction}
+          className={classes.closeButton}
+        >
           <X />
         </button>
       </div>
       <section>
         <h3>Category</h3>
-        <CategoryList isNavList={false} />
+        <CategoryList
+          isNavList={false}
+          inputChangeAction={changeCategoryHandler}
+        />
       </section>
       <section>
         <h3>Sort By</h3>
-        <SortOptions />
+        <SortOptions changeAction={changeSortByHandler} />
       </section>
       <section>
         <h3>Price Range</h3>
         <div className={classes.textInputContainer}>
-          <TextInput placeholder="Min Price" />
-          <TextInput placeholder="Max Price" />
+          <TextInput
+            placeholder="Min Price"
+            changeAction={changeMinPriceHandler}
+            value={minPrice}
+          />
+          <TextInput
+            placeholder="Max Price"
+            changeAction={changeMaxPriceHandler}
+            value={maxPrice}
+          />
         </div>
       </section>
       <Button textContent="Apply Filter" />
-    </div>
+    </form>
   );
 };
 
