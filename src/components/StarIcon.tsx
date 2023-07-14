6ceb27b1c85e979508e4802bdc0be6c3e8259586
nicolas <nicolas.lima03@hotmail.com>
2023-07-14 16:30:47 -0300

    pass a new prop that defines if the star is to be filled or not

--- src/components/StarIcon.tsx
+++ src/components/StarIcon.tsx
@@ -1,9 +1,16 @@
 interface PropType {
   height?: string;
+  filled?: boolean;
 }
 
-const StarIcon: React.FC<PropType> = ({ height }) => {
-  return (
+const StarIcon: React.FC<PropType> = ({ height, filled = true }) => {
+  return filled ? (
+    <img
+      style={{ height: height }}
+      src="src/assets/svg/star-filled.svg"
+      alt="icon of a yellow star"
+    />
+  ) : (
     <img
       style={{ height: height }}
       src="src/assets/svg/star-filled.svg"
