8465f1d75810f6e11986afaf3fd60461123f5d4a
nicolas <nicolas.lima03@hotmail.com>
2023-07-16 02:03:15 -0300

    dinamically render Review component based on prop values

--- src/components/ReviewList.tsx
+++ src/components/ReviewList.tsx
@@ -1,11 +1,17 @@
 import classes from "./ReviewList.module.css";
 import Review from "./Review";
+import { ReviewType } from "../pages/HomePage";
 
-const ReviewList = () => {
+interface PropTypes {
+  reviews: ReviewType[];
+}
+
+const ReviewList: React.FC<PropTypes> = ({ reviews }) => {
   return (
     <div className={classes.container}>
-      <Review />
-      <Review />
+      {reviews.map((review) => (
+        <Review review={review} />
+      ))}
     </div>
   );
 };
