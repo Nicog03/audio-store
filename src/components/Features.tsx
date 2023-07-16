8268f7f804c741a65855aeb17f7627182e9eb441
nicolas <nicolas.lima03@hotmail.com>
2023-07-15 23:46:10 -0300

    create features component

---
+++ src/components/Features.tsx
@@ -0,0 +1,12 @@
+const Features = () => {
+  return (
+    <p style={{ fontSize: "0.875rem" }}>
+      The speaker unit contains a diaphragm that is precision-grown from NAC
+      Audio bio-cellulose, making it stiffer, lighter and stronger than regular
+      PET speaker units, and allowing the sound-producing diaphragm to vibrate
+      without the levels of distortion found in other speakers.
+    </p>
+  );
+};
+
+export default Features;
