b1a7006a48da1f9b813deff062c240a66e1fcf7c
nicolas <nicolas.lima03@hotmail.com>
2023-07-16 23:51:16 -0300

    pass a reload triggering function as a prop to the product array

--- src/pages/ShoppingCartPage.tsx
+++ src/pages/ShoppingCartPage.tsx
@@ -3,11 +3,12 @@ import classes from "./ShoppingCartPage.module.css";
 import SearchHeader from "../components/SearchHeader";
 import Button from "../components/Button";
 import { Context } from "../App";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ProductArrayCompact from "../components/ProductArrayCompact";
 
 const ShoppingCartPage = () => {
   const { context } = useContext(Context);
+  const [value, setValue] = useState(0);
 
   let totalItems = 0;
   context.map((product) => (totalItems += product.quantity!));
@@ -18,12 +19,20 @@ const ShoppingCartPage = () => {
       (totalPrice += +product.price.replace("$", "") * product.quantity!)
   );
 
+  const updatePageHandler = () => {
+    setValue((value) => value + 1);
+  };
+
   return (
     <>
       <div className={classes.container}>
         <SearchHeader />
         <div className={classes.content}>
-          <ProductArrayCompact isOnShoppingCart={true} products={context} />
+          <ProductArrayCompact
+            updateStore={updatePageHandler}
+            isOnShoppingCart={true}
+            products={context}
+          />
           <div className={classes.bottomSection}>
             <section className={classes.totalSection}>
               <h3>Total {totalItems} Items</h3>
