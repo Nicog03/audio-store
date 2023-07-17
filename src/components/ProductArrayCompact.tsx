c9d0e27cceb535f4ab2d7b66e331c1a75ed62240
nicolas <nicolas.lima03@hotmail.com>
2023-07-16 22:37:26 -0300

    define a new prop and dinamically render a render based on it

--- src/components/ProductArrayCompact.tsx
+++ src/components/ProductArrayCompact.tsx
@@ -1,17 +1,36 @@
 import { ProductType } from "../pages/HomePage";
 import classes from "./ProductArrayCompact.module.css";
 import CompactProductCard from "./CompactProductCard";
+import { Link } from "react-router-dom";
 
 interface PropType {
   products: ProductType[];
+  isOnShoppingCart: boolean;
 }
 
-const ProductArrayCompact: React.FC<PropType> = ({ products }) => {
+const ProductArrayCompact: React.FC<PropType> = ({
+  products,
+  isOnShoppingCart,
+}) => {
   return (
     <div className={classes.container}>
-      {products.map((product) => (
-        <CompactProductCard key={product.created_at} productInfo={product} />
-      ))}
+      {products.map((product) =>
+        isOnShoppingCart ? (
+          <CompactProductCard
+            key={product.created_at}
+            productInfo={product}
+            isOnShoppingCart={isOnShoppingCart}
+          />
+        ) : (
+          <Link to={`/product/${product.id}/overview`}>
+            <CompactProductCard
+              key={product.created_at}
+              productInfo={product}
+              isOnShoppingCart={isOnShoppingCart}
+            />
+          </Link>
+        )
+      )}
     </div>
   );
 };
