9a51b8c3e9f1bd720057dc7eb74413238e64e554
nicolas <nicolas.lima03@hotmail.com>
2023-07-16 23:50:00 -0300

    define amount and total price of items & pass those values to their respective element

--- src/pages/ShoppingCartPage.tsx
+++ src/pages/ShoppingCartPage.tsx
@@ -9,6 +9,15 @@ import ProductArrayCompact from "../components/ProductArrayCompact";
 const ShoppingCartPage = () => {
   const { context } = useContext(Context);
 
+  let totalItems = 0;
+  context.map((product) => (totalItems += product.quantity!));
+
+  let totalPrice = 0;
+  context.map(
+    (product) =>
+      (totalPrice += +product.price.replace("$", "") * product.quantity!)
+  );
+
   return (
     <>
       <div className={classes.container}>
@@ -17,8 +26,8 @@ const ShoppingCartPage = () => {
           <ProductArrayCompact isOnShoppingCart={true} products={context} />
           <div className={classes.bottomSection}>
             <section className={classes.totalSection}>
-              <h3>Total 2 Items</h3>
-              <p>USD 295</p>
+              <h3>Total {totalItems} Items</h3>
+              <p>USD {totalPrice}</p>
             </section>
             <Button textContent="Proceed to Checkout" />
           </div>
