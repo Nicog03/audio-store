763fa604b1f470aab56547c1f97e2775dad3c217
nicolas <nicolas.lima03@hotmail.com>
2023-07-17 11:50:17 -0300

    implement a counter next to the shop icon

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -4,15 +4,23 @@ import { ShoppingCart } from "react-feather";
 import { Link } from "react-router-dom";
 
 import classes from "./SearchHeader.module.css";
-
+import { useContext } from "react";
+import { Context } from "../App";
 const SearchHeader = () => {
+  const { context } = useContext(Context);
+
+  let itemsQnt = 0;
+
+  context.map((product) => (itemsQnt += product.quantity!));
+
   return (
     <header className={classes.header}>
       <Link to="/headphones" className={classes.link}>
         <ChevronLeft />
       </Link>
       <h1 className={classes.heading}>Search</h1>
-      <Link to="/shopping-cart">
+      <Link className={classes.shoppingLink} to="/shopping-cart">
+        {itemsQnt != 0 && <p>{itemsQnt}</p>}
         <ShoppingCart />
       </Link>
     </header>
