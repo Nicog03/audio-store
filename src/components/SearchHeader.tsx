586528e2ae32fb75b646b9cb9b06a394beb453d4
nicolas <nicolas.lima03@hotmail.com>
2023-07-17 12:17:00 -0300

    define a new prop & render components based on that prop value

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft } from "react-feather";
+import { ChevronLeft, Trash2 } from "react-feather";
 import { ShoppingCart } from "react-feather";
 
 import { Link } from "react-router-dom";
@@ -6,23 +6,39 @@ import { Link } from "react-router-dom";
 import classes from "./SearchHeader.module.css";
 import { useContext } from "react";
 import { Context } from "../App";
-const SearchHeader = () => {
+
+interface PropType {
+  mode?: "search" | "default" | "shop";
+}
+
+const SearchHeader: React.FC<PropType> = ({ mode = "default" }) => {
   const { context } = useContext(Context);
 
   let itemsQnt = 0;
 
   context.map((product) => (itemsQnt += product.quantity!));
 
+  const searchMode = mode === "search";
+  const shopMode = mode === "shop";
+
   return (
     <header className={classes.header}>
       <Link to="/headphones" className={classes.link}>
         <ChevronLeft />
       </Link>
-      <h1 className={classes.heading}>Search</h1>
-      <Link className={classes.shoppingLink} to="/shopping-cart">
-        {itemsQnt != 0 && <p>{itemsQnt}</p>}
-        <ShoppingCart />
-      </Link>
+      {searchMode && <h1 className={classes.heading}>Search</h1>}
+      {shopMode && <h1 className={classes.heading}>Shopping Cart</h1>}
+
+      {!shopMode ? (
+        <Link className={classes.shoppingLink} to="/shopping-cart">
+          {itemsQnt != 0 && <p>{itemsQnt}</p>}
+          <ShoppingCart />
+        </Link>
+      ) : (
+        <button className={classes.trashButton}>
+          <Trash2 />
+        </button>
+      )}
     </header>
   );
 };
