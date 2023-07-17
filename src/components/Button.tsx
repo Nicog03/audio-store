2701ea24cd0a7377ed54a65ea6649d78c8034af9
nicolas <nicolas.lima03@hotmail.com>
2023-07-16 22:39:07 -0300

    define a new prop responsible for passing a click event

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,10 +2,15 @@ import classes from "./Button.module.css";
 
 interface PropType {
   textContent: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: React.FC<PropType> = ({ textContent }) => {
-  return <button className={classes.button}>{textContent}</button>;
+const Button: React.FC<PropType> = ({ textContent, onClick }) => {
+  return (
+    <button onClick={onClick} className={classes.button}>
+      {textContent}
+    </button>
+  );
 };
 
 export default Button;
