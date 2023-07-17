a81d8db812db34b18e9c535a74c48421257e1821
nicolas <nicolas.lima03@hotmail.com>
2023-07-16 22:52:14 -0300

    dinamically structure the card based on prop

--- src/components/CompactProductCard.tsx
+++ src/components/CompactProductCard.tsx
@@ -1,21 +1,44 @@
 import classes from "./CompactProductCard.module.css";
 
-import { MoreVertical } from "react-feather";
+import { Minus, Plus, MoreVertical, Trash2 } from "react-feather";
 
 import StarIcon from "./StarIcon";
 import { ProductType } from "../pages/HomePage";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Context } from "../App";
 
 interface PropType {
   productInfo: ProductType;
+  isOnShoppingCart?: boolean;
 }
 
-const CompactProductCard: React.FC<PropType> = ({ productInfo }) => {
+const CompactProductCard: React.FC<PropType> = ({
+  productInfo,
+  isOnShoppingCart = false,
+}) => {
+  const { setContext } = useContext(Context);
+  const [value, setValue] = useState(0);
+
+  if (productInfo.quantity === 0) {
+    setContext((current) => current.filter((product) => product.quantity != 0));
+  }
+
+  const updatePage = () => {
+    setValue((value) => value + 1);
+  };
+
+  const reduceQuantityHandler = () => {
+    productInfo.quantity!--;
+    updatePage();
+  };
+
+  const increaseQuantityHandler = () => {
+    productInfo.quantity!++;
+    updatePage();
+  };
+
   return (
-    <Link
-      to={`/product/${productInfo.id}/overview`}
-      className={classes.container}
-    >
+    <div className={classes.container}>
       <div className={classes.imageContainer}>
         <img src="src/assets/png/headset.png" alt="headset image" />
       </div>
@@ -24,18 +47,34 @@ const CompactProductCard: React.FC<PropType> = ({ productInfo }) => {
           <p className={classes.productTitle}>{productInfo.name}</p>
           <p className={classes.price}>USD {productInfo.price}</p>
         </div>
-        <div className={classes.bottomContainer}>
-          <div className={classes.reviewSection}>
-            <div className={classes.starSection}>
-              <StarIcon />
-              <p>{productInfo.rating}</p>
+        {!isOnShoppingCart ? (
+          <div className={classes.bottomContainer}>
+            <div className={classes.reviewSection}>
+              <div className={classes.starSection}>
+                <StarIcon />
+                <p>{productInfo.rating}</p>
+              </div>
+              <p>{productInfo.reviews.length} Reviews</p>
             </div>
-            <p>{productInfo.reviews.length} Reviews</p>
+
+            <MoreVertical className={classes.optionsIcon} />
           </div>
-          <MoreVertical className={classes.optionsIcon} />
-        </div>
+        ) : (
+          <div className={classes.shoppingBottomContainer}>
+            <div className={classes.quantityContainer}>
+              <button onClick={reduceQuantityHandler}>
+                <Minus height={"1.25rem"} width={"1.25rem"} />
+              </button>
+              {productInfo.quantity}
+              <button onClick={increaseQuantityHandler}>
+                <Plus height={"1.25rem"} width={"1.25rem"} />
+              </button>
+            </div>
+            <Trash2 height={"1.25rem"} width={"1.25rem"} />
+          </div>
+        )}
       </div>
-    </Link>
+    </div>
   );
 };
 
