01a4298e7f3e4e96bb699a105b0df895fc419e19
nicolas <nicolas.lima03@hotmail.com>
2023-07-17 22:11:39 -0300

    export needed dependencies for login with google

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,6 +20,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+export const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export default auth;
+export const googleSignIn = () => signInWithPopup(auth, provider);
