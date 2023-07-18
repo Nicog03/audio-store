3b0ae0006ed1bb7cc2911663d13f05d3b53b4ed2
nicolas <nicolas.lima03@hotmail.com>
2023-07-18 15:03:18 -0300

    export needed dependencies for auth with facebook

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,6 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  FacebookAuthProvider,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +27,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 
 export const googleSignIn = () => signInWithPopup(auth, provider);
+export const facebookSignIn = () => signInWithPopup(auth, facebookProvider);
