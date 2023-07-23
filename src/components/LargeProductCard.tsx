b91f3a26579baa5719e10f7e4ec07c5b850cad87
nicolas <nicolas.lima03@hotmail.com>
2023-07-23 20:22:39 -0300

    display an empty card when no info is received & animate opacity

--- src/components/LargeProductCard.tsx
+++ src/components/LargeProductCard.tsx
@@ -1,27 +1,42 @@
 import { Link } from "react-router-dom";
 import classes from "./LargeProductCard.module.css";
-import { ArrowRight } from "react-feather";
+import { ArrowRight, Headphones } from "react-feather";
 import { ProductType } from "../pages/HomePage";
 
+import { motion } from "framer-motion";
+
 interface propType {
-  productInfo: ProductType;
+  productInfo?: ProductType;
 }
 
 const LargeProductCard: React.FC<propType> = ({ productInfo }) => {
   return (
     <>
-      <div className={classes.container}>
-        <div className={classes.leftSide}>
-          <h2>{productInfo.name}</h2>
-          <Link
-            className={classes.link}
-            to={`/product/${productInfo.id}/overview`}
-          >
-            Shop now <ArrowRight />
-          </Link>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+        <div className={classes.container}>
+          {productInfo ? (
+            <>
+              <div className={classes.leftSide}>
+                <h2>{productInfo.name}</h2>
+                <Link
+                  className={classes.link}
+                  to={`/product/${productInfo.id}/overview`}
+                >
+                  Shop now <ArrowRight />
+                </Link>
+              </div>
+              <img src="./src/assets/png/headset.png" alt="" />
+            </>
+          ) : (
+            <div className={classes.noProductCard}>
+              <div className={classes.leftSide}>
+                <h2>No product to be displayed</h2>
+              </div>
+              <Headphones height={"5em"} width={"5em"} />
+            </div>
+          )}
         </div>
-        <img src="./src/assets/png/headset.png" alt="" />
-      </div>
+      </motion.div>
     </>
   );
 };
