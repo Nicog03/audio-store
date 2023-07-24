59ac2ecfd56d795aa9e3071cbc7a31b9945ef5f9
nicolas <nicolas.lima03@hotmail.com>
2023-07-23 21:05:25 -0300

    modify logo svg import method

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,10 @@
 import classes from "./Logo.module.css";
+import logo from "../assets/svg/audio-logo.svg";
 
 const Logo = () => {
   return (
     <div className={classes.container}>
-      <img src="src/assets/svg/audio-logo.svg" alt="audio logo" />
+      <img src={logo} alt="audio logo" />
       <h1>Audio</h1>
     </div>
   );
