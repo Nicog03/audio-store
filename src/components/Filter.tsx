a993af2334f8f0f60b36968acb41f2457917c3ce
nicolas <nicolas.lima03@hotmail.com>
2023-07-24 14:06:19 -0300

    store filter options on context & pass them as props to the input elements

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -4,7 +4,8 @@ import TextInput from "./TextInput";
 import { X } from "react-feather";
 import Button from "./Button";
 import SortOptions from "./SortOptions";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { Context } from "../App";
 
 interface FilterType {
   category: string;
@@ -26,10 +27,12 @@ const Filter: React.FC<PropType> = ({
   setFilterQnt,
   setOpen,
 }) => {
-  const [category, setCategory] = useState("");
-  const [sortBy, setSortBy] = useState("");
-  const [minPrice, setMinPrice] = useState("");
-  const [maxPrice, setMaxPrice] = useState("");
+  const { filter, setFilter } = useContext(Context);
+
+  const [category, setCategory] = useState(filter.category);
+  const [sortBy, setSortBy] = useState(filter.sortBy);
+  const [minPrice, setMinPrice] = useState(filter.minPrice);
+  const [maxPrice, setMaxPrice] = useState(filter.maxPrice);
 
   const changeCategoryHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCategory(e.target.value);
@@ -49,10 +52,19 @@ const Filter: React.FC<PropType> = ({
 
   const changeFilterOptions = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const filter = [category, sortBy, minPrice, maxPrice];
+    const FilterArray = [category, sortBy, minPrice, maxPrice];
     let numOfFilter = 0;
 
-    filter.forEach((item) => (item != "" ? numOfFilter++ : null));
+    FilterArray[1] != "" ? numOfFilter++ : null;
+    FilterArray[2] != "" ? numOfFilter++ : null;
+    FilterArray[3] != "" || FilterArray[4] != "" ? numOfFilter++ : null;
+
+    setFilter({
+      category,
+      sortBy,
+      minPrice,
+      maxPrice,
+    });
 
     setFilterQnt(numOfFilter);
     changeData({ category, sortBy, minPrice, maxPrice });
@@ -76,11 +88,12 @@ const Filter: React.FC<PropType> = ({
         <CategoryList
           isNavList={false}
           inputChangeAction={changeCategoryHandler}
+          categoryValue={category}
         />
       </section>
       <section>
         <h3>Sort By</h3>
-        <SortOptions changeAction={changeSortByHandler} />
+        <SortOptions changeAction={changeSortByHandler} current={sortBy} />
       </section>
       <section>
         <h3>Price Range</h3>
@@ -89,11 +102,13 @@ const Filter: React.FC<PropType> = ({
             type="text"
             placeholder="Min Price"
             changeAction={changeMinPriceHandler}
+            value={minPrice}
           />
           <TextInput
             type="text"
             placeholder="Max Price"
             changeAction={changeMaxPriceHandler}
+            value={maxPrice}
           />
         </div>
       </section>
