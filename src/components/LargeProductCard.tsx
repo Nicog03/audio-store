a2515818a9dea0a41929c07ecac596bccddee4a2
nicolas <nicolas.lima03@hotmail.com>
2023-07-23 21:44:20 -0300

    remove uppermost container, as it displayed no use

--- src/components/LargeProductCard.tsx
+++ src/components/LargeProductCard.tsx
@@ -13,30 +13,32 @@ interface propType {
 const LargeProductCard: React.FC<propType> = ({ productInfo }) => {
   return (
     <>
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-        <div className={classes.container}>
-          {productInfo ? (
-            <>
-              <div className={classes.leftSide}>
-                <h2>{productInfo.name}</h2>
-                <Link
-                  className={classes.link}
-                  to={`/product/${productInfo.id}/overview`}
-                >
-                  Shop now <ArrowRight />
-                </Link>
-              </div>
-              <img src={headset} alt="image of a headset" />
-            </>
-          ) : (
-            <div className={classes.noProductCard}>
-              <div className={classes.leftSide}>
-                <h2>No product to be displayed</h2>
-              </div>
-              <Headphones height={"5em"} width={"5em"} />
+      <motion.div
+        className={classes.container}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
+        {productInfo ? (
+          <>
+            <div className={classes.leftSide}>
+              <h2>{productInfo.name}</h2>
+              <Link
+                className={classes.link}
+                to={`/product/${productInfo.id}/overview`}
+              >
+                Shop now <ArrowRight />
+              </Link>
             </div>
-          )}
-        </div>
+            <img src={headset} alt="image of a headset" />
+          </>
+        ) : (
+          <div className={classes.noProductCard}>
+            <div className={classes.leftSide}>
+              <h2>No product to be displayed</h2>
+            </div>
+            <Headphones height={"5em"} width={"5em"} />
+          </div>
+        )}
       </motion.div>
     </>
   );
